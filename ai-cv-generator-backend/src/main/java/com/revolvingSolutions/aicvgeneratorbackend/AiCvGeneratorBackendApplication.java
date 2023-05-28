8634348079a9111a72c8f7f1bc13d15bd067b607
nathanMils <106823768+nathanMils@users.noreply.github.com>
2023-05-28 21:05:40 +0200

    (api) added Basic user class

--- ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/AiCvGeneratorBackendApplication.java
+++ ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/AiCvGeneratorBackendApplication.java
@@ -1,20 +1,12 @@
-package com.revolvingSolutions.aicvgeneratorbackend.application;
+package com.revolvingSolutions.aicvgeneratorbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class AiCvGeneratorBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AiCvGeneratorBackendApplication.class, args);
-	}
-
-	@GetMapping(value="/")
-	public String hello() {
-		return "Hello world";
 	}
 }
---
+++ ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.revolvingSolutions.aicvgeneratorbackend.controller;
+
+import com.revolvingSolutions.aicvgeneratorbackend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/User")
+public class UserController {
+    private final UserService service;
+
+    @Autowired
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public String getUserName() {
+        return service.getUsername();
+    }
+}
