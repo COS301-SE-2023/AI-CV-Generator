ca85d9e4bd1a7453900ef77ea00587b63e6b79e5
u19284072 <u19284072@tuks.co.za>
2023-05-30 22:14:26 +0200

    Create template profile widget

---
+++ fe/lib/pages/profile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Profile extends StatefulWidget {
+  @override
+  ProfileState createState() => ProfileState();
+}
+
+class ProfileState extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('');
+  }
+}
