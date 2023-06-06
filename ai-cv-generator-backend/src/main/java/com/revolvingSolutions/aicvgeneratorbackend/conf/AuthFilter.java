e265f3ecce44e3045622d557ccfbfa6a543562ca
nathanMils <106823768+nathanMils@users.noreply.github.com>
2023-06-06 23:49:47 +0200

    🚧 (api) auth service update

--- ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/conf/AuthFilter.java
+++ ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/conf/AuthFilter.java
@@ -1,12 +1,16 @@
 package com.revolvingSolutions.aicvgeneratorbackend.conf;
 
 import com.revolvingSolutions.aicvgeneratorbackend.service.AuthService;
+import com.revolvingSolutions.aicvgeneratorbackend.service.UserDetailService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,7 +20,8 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class AuthFilter extends OncePerRequestFilter {
 
-    private AuthService service;
+    private AuthService authService;
+    private UserDetailService uDetailService;
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
@@ -26,6 +31,12 @@ public class AuthFilter extends OncePerRequestFilter {
         final String header = request.getHeader("Authorization");
         if (header != null && header.startsWith("Bearer")) {
             final String token = header.substring(7);
+            final String username = authService.getUsername(token);
+            if (username != null) {
+                if (SecurityContextHolder.getContext().getAuthentication() == null) {
+                    //UserDetails details = this. tbc
+                }
+            }
 
         } else {
             filterChain.doFilter(request,response);
