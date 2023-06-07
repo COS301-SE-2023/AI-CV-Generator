c0f32912cc6aed1a82dc5dd19ca5d1ccacd200ac
nathanMils <106823768+nathanMils@users.noreply.github.com>
2023-06-07 15:49:05 +0200

    🚧 (api) working on token storage in the fe

--- fe/macos/Flutter/GeneratedPluginRegistrant.swift
+++ fe/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import device_info_plus_macos
+import flutter_secure_storage_macos
 import pdfx
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   PdfxPlugin.register(with: registry.registrar(forPlugin: "PdfxPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
