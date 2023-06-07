66f07832d266a38d66a8c7a36b7bda1c3a54586d
nathanMils <106823768+nathanMils@users.noreply.github.com>
2023-06-07 12:38:22 +0200

    ✨ (api) authentication config complete

--- ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/conf/appConf.java
+++ ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/conf/appConf.java
@@ -4,9 +4,15 @@ import com.revolvingSolutions.aicvgeneratorbackend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -18,6 +24,28 @@ public class appConf {
         return username -> repository.findByUsername(username).orElseThrow(
                 () -> new UsernameNotFoundException("User Does not exist")
         );
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(
+                userDetailsService()
+        );
+        provider.setPasswordEncoder(
+                passEncoder()
+        );
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration conf) throws Exception {
+        return conf.getAuthenticationManager();
     }
 
 }
