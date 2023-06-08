c5daa1c1796a67a46374970bc8f400a3a15f919c
Amanda9805 <akhuzwayo98@gmail.com>
2023-06-08 23:08:25 +0200

    Revert "cv repository"
    This reverts commit eb22b428a7f373fc5abf3aa9b1b7a3e007de22c2.

--- ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/repository/CvRepository.java
+++
@@ -1,9 +0,0 @@
-package com.revolvingSolutions.aicvgeneratorbackend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.revolvingSolutions.aicvgeneratorbackend.entitiy.CvEntity;
-
-public interface CvRepository extends JpaRepository<CvEntity, String> {
-    
-}
