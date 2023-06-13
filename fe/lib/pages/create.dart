5cfd19c97aa121fb54c97e5548246964bcbfde6c
Amanda9805 <akhuzwayo98@gmail.com>
2023-06-14 01:58:29 +0200

    Update create.dart

--- fe/lib/pages/create.dart
+++ fe/lib/pages/create.dart
@@ -123,6 +123,31 @@ class PersonalDetailsFormState extends State<PersonalDetailsForm> {
               ),
             ),
           ),
+
+                    Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center (
+              child: ConstrainedBox(
+              constraints: BoxConstraints.tight(const Size(550,50)),
+                child: TextFormField(
+                // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  
+                  decoration: const InputDecoration(  
+                    icon: Icon(Icons.home),  
+                    hintText: 'Enter your address',  
+                    labelText: 'Address',  
+                    enabledBorder: OutlineInputBorder(),
+                  ), 
+                ),
+              ),
+            ),
+          ),
           
           Padding(
             padding: const EdgeInsets.all(20.0),
