5a1762f40643f57cd88cfff8c94dbeb691212450
Amanda9805 <akhuzwayo98@gmail.com>
2023-06-14 01:55:38 +0200

    Update create.dart

--- fe/lib/pages/create.dart
+++ fe/lib/pages/create.dart
@@ -59,7 +59,14 @@ class PersonalDetailsFormState extends State<PersonalDetailsForm> {
               child: ElevatedButton(
                 onPressed: () {
                   // Validate returns true if the form is valid, or false otherwise.
-                  
+                  if (_formKey.currentState!.validate()) {
+                    // If the form is valid, display a snackbar. In the real world,
+                    // you'd often call a server or save the information in a database.
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Processing Data')),
+                    );
+                  }
+
                   Navigator.push(
                   context,
                     MaterialPageRoute(builder: (context) => const QualificationsForm()),
