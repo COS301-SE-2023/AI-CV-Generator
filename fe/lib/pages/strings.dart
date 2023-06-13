514ebe6fd97490a142ae02a20e69c3326cc77635
Amanda9805 <akhuzwayo98@gmail.com>
2023-06-14 01:45:15 +0200

    Create strings.dart

---
+++ fe/lib/pages/strings.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class StringsPersonal {
+  static final appBarTitle="AI CV Generator";
+  static final appHeadingTitle="Create CV";
+  static final appsubHeadingTitle="Section A: Personal Details";
+}
+
