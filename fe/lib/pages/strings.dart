663b53027b4d659878d5732bc8a0075358e8db21
Amanda9805 <akhuzwayo98@gmail.com>
2023-06-14 01:45:44 +0200

    Update strings.dart

--- fe/lib/pages/strings.dart
+++ fe/lib/pages/strings.dart
@@ -6,3 +6,9 @@ class StringsPersonal {
   static final appsubHeadingTitle="Section A: Personal Details";
 }
 
+class StringsQualifications {
+  static final appBarTitle="AI CV Generator";
+  static final appHeadingTitle="Create CV";
+  static final appsubHeadingTitle="Section B: Qualifications";
+}
+
