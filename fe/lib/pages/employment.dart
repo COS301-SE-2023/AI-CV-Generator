471b44a3154f104c8e60ac949fdfa0de3f64bab9
Amanda9805 <akhuzwayo98@gmail.com>
2023-06-18 22:37:34 +0200

    Create employment.dart

---
+++ fe/lib/pages/employment.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:ai_cv_generator/pages/strings.dart';
+import 'package:flutter/material.dart';
+
+void main () => runApp(EmploymentSection());
+
+class EmploymentForm extends StatefulWidget {
+  const EmploymentForm({super.key});
+
+  @override
+  EmploymentFormState createState() {
+    return EmploymentFormState();
+  }
+}
+
+// Create a corresponding State class.
+// This class holds data related to the form.
