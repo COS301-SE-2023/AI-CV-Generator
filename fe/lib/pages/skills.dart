57e61f14a9b3fa0f3174495f159a34358180d600
Amanda9805 <akhuzwayo98@gmail.com>
2023-06-18 22:42:45 +0200

    Create skills.dart

---
+++ fe/lib/pages/skills.dart
@@ -0,0 +1,12 @@
+import 'package:ai_cv_generator/pages/strings.dart';
+import 'package:flutter/material.dart';
+
+void main () => runApp(SkillSection());
+
+class SkillForm extends StatefulWidget {
+  const SkillForm({super.key});
+
+  @override
+  SkillFormState createState() {
+    return SkillFormState();
+  }
