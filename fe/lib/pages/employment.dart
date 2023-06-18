23aed590a22c75cf55bed7889e36dbdc6538a870
Amanda9805 <akhuzwayo98@gmail.com>
2023-06-18 22:38:41 +0200

    Update employment.dart

--- fe/lib/pages/employment.dart
+++ fe/lib/pages/employment.dart
@@ -25,5 +25,124 @@ class EmploymentFormState extends State<EmploymentForm> {
   TextEditingController startDate = TextEditingController();
   TextEditingController endDate = TextEditingController();
 
-  //MaterialApp
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget> [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center (
+              child: ConstrainedBox(
+                constraints: BoxConstraints.tight(const Size(550,50)),
+                child: TextFormField(
+                  controller: company,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(  
+                    icon: Icon(Icons.work_history),
+                    labelText: 'Company',
+                    enabledBorder: OutlineInputBorder(),
+                  ), 
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center (
+              child: ConstrainedBox(
+                constraints: BoxConstraints.tight(const Size(550,50)),
+                child: TextFormField(
+                  controller: jobTitle,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(  
+                    icon: Icon(Icons.title),  
+                    labelText: 'Job Title',
+                    enabledBorder: OutlineInputBorder(),
+                  ), 
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center (
+              child: ConstrainedBox(
+                constraints: BoxConstraints.tight(const Size(550,50)),
+                child: TextFormField(
+                  controller: startDate,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(  
+                    icon: Icon(Icons.calendar_month),
+                    labelText: 'Employment Start Date',
+                    enabledBorder: OutlineInputBorder(),
+                  ), 
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center (
+              child: ConstrainedBox(
+                constraints: BoxConstraints.tight(const Size(550,50)),
+                child: TextFormField(
+                  controller: endDate,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(  
+                    icon: Icon(Icons.calendar_month),
+                    labelText: 'Employment End Date',
+                    enabledBorder: OutlineInputBorder(),
+                  ), 
+                ),
+              ),
+            ),
+          ),
+
+
+          Padding (
+            padding: const EdgeInsets.all(8.0),
+            child: Center (
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Save & Proceed'),
+              ),
+            ),
+          ),
+
+
+        ],
+      ),
+    );
+  }
+}
+
+
+//MaterialApp
 }
