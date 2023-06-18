8be4be1127d022c90fff2b138551a60bdcd41559
Amanda9805 <akhuzwayo98@gmail.com>
2023-06-18 22:22:47 +0200

    Create education.dart

---
+++ fe/lib/pages/education.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:ai_cv_generator/pages/strings.dart';
+import 'package:flutter/material.dart';
+
+void main () => runApp(CreateEducation());
+
+class EducationDetailsForm extends StatefulWidget {
+  const EducationDetailsForm({super.key});
+
+  @override
+  EducationDetailsFormState createState() {
+    return EducationDetailsFormState();
+  }
+}
+
+// Create a corresponding State class.
+// This class holds data related to the form.
+
