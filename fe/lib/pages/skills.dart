455fbf1ac265fba1051644977af10abe0a787b3e
Amanda9805 <akhuzwayo98@gmail.com>
2023-06-19 19:46:52 +0200

    Update skills.dart

--- fe/lib/pages/skills.dart
+++ fe/lib/pages/skills.dart
@@ -1,90 +1,53 @@
 // ignore_for_file: must_be_immutable
 
+import 'package:ai_cv_generator/pages/employment.dart';
+import 'package:ai_cv_generator/pages/references.dart';
 import 'package:ai_cv_generator/pages/strings.dart';
 import 'package:flutter/material.dart';
 
-void main () => runApp(SkillSection());
+void main () => runApp(const Skills());
 
-class SkillForm extends StatefulWidget {
-  const SkillForm({super.key});
-
-  @override
-  SkillFormState createState() {
-    return SkillFormState();
-  }
-}
-
-class SkillFormState extends State<SkillForm> {
-
-  final _formKey = GlobalKey<FormState>();
-  TextEditingController skill = TextEditingController();
+class Skills extends StatelessWidget {
+  const Skills({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget> [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Center (
-              child: ConstrainedBox(
-                constraints: BoxConstraints.tight(const Size(550,50)),
-                child: TextFormField(
-                  // The validator receives the text that the user has entered.
-                  controller: skill,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter some text';
-                    }
-                    return null;
-                  },
-                  decoration: const InputDecoration(  
-                    icon: Icon(Icons.settings),  
-                    //hintText: 'Enter your name',  
-                    labelText: 'Skill',
-                    enabledBorder: OutlineInputBorder(),
-                  ), 
-                ),
-              ),
-            ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Center (
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('Save & Proceed'),
-              ),
-            ),
-          ),
-        
-        ],
-      ),
+    return const MaterialApp(
+      title: StringsSkill.appBarTitle,
+      home: SkillsForm(),
     );
   }
 }
 
+class SkillsForm extends StatefulWidget {
+  const SkillsForm({super.key});
 
-class SkillSection extends StatelessWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _SkillsFormState();
+  }
+}
 
-  //titleSection widget
-    Widget titleSection=const Column (
+class _SkillsFormState extends State<SkillsForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  TextEditingController skill = TextEditingController();
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(StringsSkill.appBarTitle),
+      ),
+      body: Center (
+        child: _buildForm(),
+      )
+    );
+  }
+
+  Widget titleSection=const Column (
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: <Widget> [
-        Padding (
-          padding: EdgeInsets.all(8.0),
-            child: Text (
-              StringsSkill.appHeadingTitle,
-              style: TextStyle (
-                fontSize: 30.0,
-            ),
-          ),
-        ),
-        
         Padding (
           padding: EdgeInsets.all(8.0),
             child: Text (
@@ -94,28 +57,95 @@ class SkillSection extends StatelessWidget {
               ),
           ),
         ),
-        
       ],
     );
 
-  SkillSection({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        title: StringsSkill.appBarTitle,
-        home: Scaffold (
-          appBar: AppBar(
-            title: const Text(StringsSkill.appBarTitle),
-          ),
-          body: ListView(
-            children: <Widget>[
-              titleSection,
-              const SkillForm(),
-            ],
-          ),
+  Widget _buildForm() {
+    return Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            titleSection,
+            _buildSkillField(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget> [
+                Container(
+                  padding: const EdgeInsets.all(8.0),
+                  child: _buildBackButton(),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8.0),
+                  child: _buildSubmitButton(),
+                ),
+              ],
+            ),
+          ],
+        ));
+  }
+
+  Widget _buildSkillField() {
+    return Container (
+      padding: const EdgeInsets.all(8.0),
+      constraints: BoxConstraints.tight(const Size(550,65)),
+      child: TextFormField(
+        controller: skill,
+        decoration: const InputDecoration(
+          labelText: 'Skill',
+          enabledBorder: OutlineInputBorder(),
         ),
-      );
-  }//MaterialApp
-}
+        // ignore: body_might_complete_normally_nullable
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Please enter some text';
+          }
+          return null;
+        },
+      )
+    );
+  }
 
+
+  Widget _buildBackButton() {
+    return SizedBox(
+      width: 150,
+      height: 30,
+      child: ElevatedButton(
+        onPressed: () {
+            _submitBack();
+          },
+          child: const Text('Back'),
+      )
+    );
+  }
+
+  Widget _buildSubmitButton() {
+    return SizedBox(
+      width: 150,
+      height: 30,
+      child: ElevatedButton(
+        onPressed: () {
+            _submitForm();
+          },
+          child: const Text('Save & Proceed'),
+      )
+    );
+    
+  }
+
+  void _submitBack() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const EmploymentDetails()));
+  }
+
+  void _submitForm() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const References()));
+  }
+}
