ccc4e27b138dd65ed7e7437ec8db4549cac43947
Amanda9805 <akhuzwayo98@gmail.com>
2023-06-22 13:33:21 +0200

    Update CvService.java

--- ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/service/CvService.java
+++ ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/service/CvService.java
@@ -2,13 +2,45 @@ package com.revolvingSolutions.aicvgeneratorbackend.service;
 
 import org.springframework.stereotype.Service;
 
+import com.revolvingSolutions.aicvgeneratorbackend.entitiy.CvEntity;
+import com.revolvingSolutions.aicvgeneratorbackend.repository.CvRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+
 @Service
 public class CvService {
-    public String getName() {
+
+    @Autowired
+    CvRepository cvRepository;
+
+    public void addCV(CvEntity cv)
+    {
+        cvRepository.save(cv);
+    }
+
+    public void deleteCV(CvEntity cv)
+    {
+        cvRepository.delete(cv);
+    }
+
+    public void updateCV(CvEntity cv)
+    {
+        try{
+            if(((CvRepository) cv).existsById(cv.getUserid()))
+            {
+                cvRepository.save(cv);
+            }
+        }
+        catch(Exception e){}
+    }
+
+    /*public String getName() {
         return "Name";
     }
 
     public String getLastname() {
         return "Lastname";
-    }
+    }*/
 }
