bb6a27febe07381a8fc3f5635ea765035b588651
nathanMils <106823768+nathanMils@users.noreply.github.com>
2023-06-25 17:13:34 +0200

    💚 (ui) change

--- fe/lib/pages/generatedCV.dart
+++ fe/lib/pages/generatedCV.dart
@@ -1,5 +1,6 @@
 
 import 'package:ai_cv_generator/dio/client/fileApi.dart';
+import 'package:ai_cv_generator/pages/create_CV.dart';
 import 'package:ai_cv_generator/pages/pdf_window.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
@@ -38,7 +39,7 @@ class _generatedCVState extends State<generatedCV> {
 
   Map data = content;
   final TextEditingController _controller = TextEditingController();
-
+  final pdf = pw.Document();
   void createCV() {
     String response = '';
     setState(() {
@@ -49,9 +50,27 @@ class _generatedCVState extends State<generatedCV> {
     });
     _controller.text = response;
   }
-
-  
-
+  PlatformFile? file;
+  Future savePdf() async {
+    Uint8List bytes = await pdf.save();
+    file = PlatformFile(name: "gen.pdf", size: bytes.length,bytes: bytes);
+    // await FileApi.uploadFile(file: file);
+  }
+  writeOnPdf() {
+    pdf.addPage(
+      pw.MultiPage(
+        pageFormat: PdfPageFormat.a4,
+        margin: const pw.EdgeInsets.all(32),
+        build: (pw.Context context) {
+          return <pw.Widget>[
+            pw.Text(
+              _controller.text
+            )
+          ];
+        },
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,12 +86,7 @@ class _generatedCVState extends State<generatedCV> {
                   child: OutlinedButton(
                     onPressed: () async {
                       setState(() {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const MyAppforPDf()
-                          ),
-                        );
+                        createCV();
                       });
                     },
                     child: const Text("Generate")
@@ -82,12 +96,6 @@ class _generatedCVState extends State<generatedCV> {
               
               Expanded(
                 child: Container(
-                  // decoration: BoxDecoration(
-                  // border: Border.all(
-                  //   color: Colors.grey,
-                  //   width: 1.0,
-                  // ),
-                // ),
                   child: ListView(
                     padding: const EdgeInsets.all(16.0),
                     children: [
@@ -112,102 +120,24 @@ class _generatedCVState extends State<generatedCV> {
               child: const Icon(Icons.download),
             ),
           ),
+          Positioned(
+            bottom: 18.0,
+            left: 18.0,
+            child: FloatingActionButton(
+              onPressed: () async {
+                writeOnPdf();
+                await savePdf();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => PdfWindow(file: file,)
+                  ),
+                );
+              },
+            ),
+          )
         
         ],
-      ),
-    );
-  }
-}
-class MyAppforPDf extends StatelessWidget {
-  const MyAppforPDf({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: PdfTest(),
-    );
-  }
-}
-
-class PdfTest extends StatelessWidget {
-  final pdf = pw.Document();
-
-  PdfTest({super.key});
-  writeOnPdf() {
-    pdf.addPage(
-      pw.MultiPage(
-        pageFormat: PdfPageFormat.a4,
-        margin: const pw.EdgeInsets.all(32),
-        build: (pw.Context context) {
-          return <pw.Widget>[
-            pw.Header(
-              level: 0,
-              child: pw.Text("1st header"),
-            ),
-            pw.Paragraph(text: "TEST PDF"),
-            pw.Paragraph(text: "TEST PDF"),
-            pw.Paragraph(text: "TEST PDF"),
-            pw.Paragraph(text: "TEST PDF"),
-            pw.Paragraph(text: "TEST PDF"),
-            pw.Paragraph(text: "TEST PDF"),
-            pw.Header(level: 1, child: pw.Text("2nd ehader")),
-            pw.Paragraph(text: "TEST PDF"),
-            pw.Paragraph(text: "TEST PDF"),
-            pw.Paragraph(text: "TEST PDF"),
-            pw.Paragraph(text: "TEST PDF"),
-            pw.Paragraph(text: "TEST PDF"),
-            pw.Paragraph(text: "TEST PDF"),
-            pw.Paragraph(text: "TEST PDF"),
-            pw.Paragraph(text: "TEST PDF"),
-            pw.Paragraph(text: "TEST PDF"),
-          ];
-        },
-      ),
-    );
-  }
-  PlatformFile? file;
-  Future savePdf() async {
-    Uint8List bytes = await pdf.save();
-    file = PlatformFile(name: "gen.pdf", size: bytes.length,bytes: bytes);
-    await FileApi.uploadFile(file: file);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Temporary page"),
-      ),
-      body: const SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Generator",
-              style: TextStyle(fontSize: 34),
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          writeOnPdf();
-          await savePdf();
-
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => PdfWindow(file: file,)
-            ),
-          );
-        },
-        child: const Icon(Icons.save),
       ),
     );
   }
