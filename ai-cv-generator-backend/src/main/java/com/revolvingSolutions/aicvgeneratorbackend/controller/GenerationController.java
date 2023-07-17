bba6effc8b71d31487e1118ad91085dd791d9e1b
nathanMils <106823768+nathanMils@users.noreply.github.com>
2023-07-17 23:18:08 +0200

    (api) Added Controller and model for CV data

--- ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/controller/GenerationController.java
+++ ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/controller/GenerationController.java
@@ -1,6 +1,16 @@
 package com.revolvingSolutions.aicvgeneratorbackend.controller;
 
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 //Will have to do with actual generation of the CV
+@RestController
+@CrossOrigin(value="*")
+@RequestMapping(path = "/generate")
+@RequiredArgsConstructor
 public class GenerationController {
+
 }
