bb7a082c60aa992f5496b0a1c651b8ff8969277c
nathanMils <106823768+nathanMils@users.noreply.github.com>
2023-07-17 23:16:33 +0200

    (api) Added request and response for cv generation

--- ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/service/GenerationService.java
+++ ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/service/GenerationService.java
@@ -1,6 +1,27 @@
 package com.revolvingSolutions.aicvgeneratorbackend.service;
 
 
+import com.revolvingSolutions.aicvgeneratorbackend.model.CVData;
+import com.revolvingSolutions.aicvgeneratorbackend.request.generation.GenerationRequest;
+import com.revolvingSolutions.aicvgeneratorbackend.response.generation.GenerationResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
 //This class will query OpenAI api to build CV with generated subsections
+@Service
+@RequiredArgsConstructor
 public class GenerationService {
+    @Value("${app.openAIKey}")
+    private String OpenAIKey;
+
+    public GenerationResponse generateCV(GenerationRequest request) {
+        return GenerationResponse.builder()
+                .cvData(
+                        CVData.builder()
+                                .build()
+                )
+                .build();
+    }
+
 }
