7442b81f4dbb2405123cd164bc14fa864f4f689e
nathanMils <106823768+nathanMils@users.noreply.github.com>
2023-07-18 15:57:42 +0200

    Revert "(api) Added RestTemplate for api calls on BE"
    This reverts commit 1cb16afe149d885ecec8b59c5cafe98a3b58760c.

--- ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/AiCvGeneratorBackendApplication.java
+++ ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/AiCvGeneratorBackendApplication.java
@@ -2,18 +2,11 @@ package com.revolvingSolutions.aicvgeneratorbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class AiCvGeneratorBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AiCvGeneratorBackendApplication.class, args);
-	}
-
-	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
 	}
 }
--- ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/service/GenerationService.java
+++ ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/service/GenerationService.java
@@ -5,10 +5,8 @@ import com.revolvingSolutions.aicvgeneratorbackend.model.CVData;
 import com.revolvingSolutions.aicvgeneratorbackend.request.generation.GenerationRequest;
 import com.revolvingSolutions.aicvgeneratorbackend.response.generation.GenerationResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 //This class will query OpenAI api to build CV with generated subsections
 @Service
@@ -16,12 +14,6 @@ import org.springframework.web.client.RestTemplate;
 public class GenerationService {
     @Value("${app.openAIKey}")
     private String OpenAIKey;
-
-    @Value("${app.api.host.baseurl}")
-    private String baseurl;
-
-    @Autowired
-    private RestTemplate restTemplate;
 
     public GenerationResponse generateCV(GenerationRequest request) {
         return GenerationResponse.builder()
