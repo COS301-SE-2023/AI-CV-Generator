60de00da9b98f37f025848fd5df691c4132af10e
nathanMils <106823768+nathanMils@users.noreply.github.com>
2023-07-18 16:00:30 +0200

    (api) Added RestTemplate for api calls on BE

--- ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/service/GenerationService.java
+++ ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/service/GenerationService.java
@@ -1,12 +1,12 @@
 package com.revolvingSolutions.aicvgeneratorbackend.service;
-
-
 import com.revolvingSolutions.aicvgeneratorbackend.model.CVData;
 import com.revolvingSolutions.aicvgeneratorbackend.request.generation.GenerationRequest;
 import com.revolvingSolutions.aicvgeneratorbackend.response.generation.GenerationResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 //This class will query OpenAI api to build CV with generated subsections
 @Service
@@ -14,6 +14,12 @@ import org.springframework.stereotype.Service;
 public class GenerationService {
     @Value("${app.openAIKey}")
     private String OpenAIKey;
+
+    @Value("${app.api.host.baseurl}")
+    private String baseurl;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     public GenerationResponse generateCV(GenerationRequest request) {
         return GenerationResponse.builder()
@@ -26,3 +32,4 @@ public class GenerationService {
     }
 
 }
+
