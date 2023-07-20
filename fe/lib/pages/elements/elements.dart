2e490ae91c1ef41c190bd998ebb35f89ba8178d4
u19284072 <u19284072@tuks.co.za>
2023-07-20 16:05:47 +0200

    added sectiontextformfield

--- fe/lib/pages/elements/elements.dart
+++ fe/lib/pages/elements/elements.dart
@@ -16,7 +16,6 @@ class SectionHeadingState extends State<SectionHeading> {
     return Align(
       alignment: widget.alignment != null? widget.alignment!: Alignment.topLeft,
       child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 16.0),
         child: Column(
           children: [
             Text(
@@ -26,7 +25,7 @@ class SectionHeadingState extends State<SectionHeading> {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            // SizedBox(height: 16),
+            SizedBox(height: 16),
           ],
         )
       )
@@ -51,6 +50,23 @@ class SectionContainerState extends State<SectionContainer> {
         borderRadius: BorderRadius.circular(20),
         color: backdrop
       ),
+      child: widget.child,
+    );
+  }
+}
+
+class SectionTextFormField extends StatefulWidget {
+  final child;
+  SectionTextFormField({super.key, required this.child});
+
+  @override
+  SectionTextFormFieldState createState() => SectionTextFormFieldState();
+}
+
+class SectionTextFormFieldState extends State<SectionTextFormField> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
       child: widget.child,
     );
   }
