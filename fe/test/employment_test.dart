87ba31318c324e0edd2c6c82283af90fbf46155f
Amanda9805 <akhuzwayo98@gmail.com>
2023-07-29 21:53:49 +0200

    widget test temporary changes

--- fe/test/employment_test.dart
+++ fe/test/employment_test.dart
@@ -1,4 +1,6 @@
+import 'package:ai_cv_generator/pages/employment2.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main(){
--- fe/test/references_test.dart
+++ fe/test/references_test.dart
@@ -1,4 +1,6 @@
+import 'package:ai_cv_generator/pages/references2.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main(){
