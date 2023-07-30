8ff0f7bc9ba703e64ae5222d4116b9d0afbec16c
u19284072 <u19284072@tuks.co.za>
2023-07-30 11:48:31 +0200

    updated template format

--- fe/lib/pages/template/TemplateA.dart
+++ fe/lib/pages/template/TemplateA.dart
@@ -1,7 +1,9 @@
 import 'dart:typed_data';
 
+import 'package:ai_cv_generator/models/user/UserModel.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 
 import '../widgets/pdf_window.dart';
@@ -31,8 +33,132 @@ class TemplateAPdf {
   final pw.Widget relatedSpacing = pw.SizedBox(height: 8);
   final pw.Widget unRelatedSpacing = pw.SizedBox(height: 16);
 
-  void writeOnPdf() async {
+  void writeOnPdf(UserModel user) async {
+    pdf.addPage(
+     pw.MultiPage(
+        pageFormat: PdfPageFormat.a4,
+        margin: const pw.EdgeInsets.all(32),
+        build: (pw.Context context) {
+          return <pw.Widget>[
+            pw.Expanded(child: 
+              pw.Row(
+                children: [
+                  pw.Expanded(
+                    child: pw.Container(
+                      color: PdfColors.lightGreen50,
+                      child: pw.Column(
+                        mainAxisAlignment: pw.MainAxisAlignment.center,
+                        children: [
+                          pw.Text(user.fname + " " + user.lname, style: pw.TextStyle(fontSize: 32)),
+                          pw.SizedBox(height: 32),
+                          pw.Row(
+                            mainAxisAlignment: pw.MainAxisAlignment.center,
+                            children: [
+                              pw.Text(user.location! + " | "),
+                              pw.Text(user.phoneNumber! + " | "),
+                              pw.Text(user.email!),
+                            ]
+                          )
+                        ]
+                      )
+                      )
+                  )
+                ]
+              )
+            ),
+            pw.Expanded(
+              flex: 5,
+              child: pw.Padding(
+                padding: pw.EdgeInsets.all(32),
+                child: pw.Row(
+                  children: [
+                    pw.Expanded( child:
+                      pw.Container(
+                        alignment: pw.Alignment.topLeft,
+                        child: pw.Column(
+                          crossAxisAlignment: pw.CrossAxisAlignment.start,
+                          children: [
+                            pw.Text("Professional Summary", style: pw.TextStyle(fontSize: 24, color: PdfColors.lightGreen100,)),
+                            pw.SizedBox(height: 8),
+                            pw.Text(user.description!),
 
+                            pw.SizedBox(height: 48),
+
+                            pw.Text("Experience", style: pw.TextStyle(fontSize: 24, color: PdfColors.lightGreen100,)),
+                            pw.SizedBox(height: 8),
+                            pw.Column(
+                              children: [
+                                pw.ListView.builder(
+                                  itemCount: user.employmenthistory!.length,
+                                  itemBuilder: ((context, index) {
+                                    return pw.Align(
+                                      alignment: pw.Alignment.centerLeft,
+                                      child: pw.Column(
+                                        crossAxisAlignment: pw.CrossAxisAlignment.start,
+                                        mainAxisAlignment: pw.MainAxisAlignment.start,
+                                        children: [
+                                          pw.Row(
+                                            children: [
+                                              pw.Text(user.employmenthistory![index].title + " | "),
+                                              pw.Text(user.employmenthistory![index].startdate.year.toString() + " - "),
+                                              pw.Text(user.employmenthistory![index].enddate.year.toString()),
+                                            ],
+                                          ),
+                                          pw.SizedBox(height: 8),
+                                          pw.Text(user.employmenthistory![index].company),
+                                          pw.SizedBox(height: 24),
+                                        ]
+                                      )
+                                    );
+                                  }),
+                                )
+                              ]
+                            ),
+
+                            pw.SizedBox(height: 48),
+
+                            pw.Text("Qualifications", style: pw.TextStyle(fontSize: 24, color: PdfColors.lightGreen100,)),
+                            pw.SizedBox(height: 8),
+                            pw.Column(
+                              children: [
+                                pw.ListView.builder(
+                                  itemCount: user.qualifications!.length,
+                                  itemBuilder: ((context, index) {
+                                    return pw.Align(
+                                      alignment: pw.Alignment.centerLeft,
+                                      child: pw.Column(
+                                        crossAxisAlignment: pw.CrossAxisAlignment.start,
+                                        mainAxisAlignment: pw.MainAxisAlignment.start,
+                                        children: [
+                                          pw.Row(
+                                            children: [
+                                              pw.Text(user.qualifications![index].intstitution + " | "),
+                                              pw.Text(user.qualifications![index].date.year.toString() + " - "),
+                                              pw.Text(user.qualifications![index].endo.year.toString()),
+                                            ],
+                                          ),
+                                          pw.SizedBox(height: 8),
+                                          pw.Text(user.qualifications![index].qualification),
+                                          pw.SizedBox(height: 24),
+                                        ]
+                                      )
+                                    );
+                                  }),
+                                )
+                              ]
+                            ),
+                          ]
+                        )
+                      )
+                    )
+                  ]
+                )
+              )
+            ),
+          ];
+        }
+     )
+    );
   }
   Future<PlatformFile> getPdf() async {
       Uint8List bytes = await pdf.save();
