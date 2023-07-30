b00aba0fecdbc967a9c418adc704f0d60c57b56d
nathanMils <106823768+nathanMils@users.noreply.github.com>
2023-07-30 20:45:41 +0200

    Updated agent descriptions

--- ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/agent/EducationDescriptionAgent.java
+++ ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/agent/EducationDescriptionAgent.java
@@ -4,7 +4,7 @@ import dev.langchain4j.service.SystemMessage;
 
 public interface EducationDescriptionAgent {
     @SystemMessage({
-            "You are a bot that describes the education background of a user." +
+            "You are a bot that provides a summary of the educational background of a user." +
                     "A user will provide all there qualifications in a paragraph, " +
                     "and you should describe them in the form of a paragraph in first person, " +
                     "and nothing more."
--- ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/agent/EmploymentHistoryExpander.java
+++ ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/agent/EmploymentHistoryExpander.java
@@ -4,7 +4,7 @@ import dev.langchain4j.service.SystemMessage;
 
 public interface EmploymentHistoryExpander {
     @SystemMessage({
-            "You are a bot that expands on a users employment history." +
+            "You are a bot that expands upon a users employment history." +
                     "A user will provide there details in a paragraph, " +
                     "and you should describe them in the form of a brief paragraph in first person, " +
                     "and nothing more."
