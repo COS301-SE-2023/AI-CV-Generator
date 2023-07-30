6d696ff9f8824fa258118d87240374a72d3ad40a
nathanMils <106823768+nathanMils@users.noreply.github.com>
2023-07-30 20:19:25 +0200

    ✨ (ui) added DescriptionForm

---
+++ fe/lib/pages/widgets/description.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class DescriptionForm extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => DescriptionFormState();
+}
+
+class DescriptionFormState extends State<DescriptionForm> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
