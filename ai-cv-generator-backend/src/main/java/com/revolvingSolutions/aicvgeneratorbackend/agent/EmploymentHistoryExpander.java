036a197507b494b0b4be566d52fdd7cf8d144f3d
nathanMils <106823768+nathanMils@users.noreply.github.com>
2023-07-30 15:03:13 +0200

    🐛 (AI) adjusted ai responses to first person

--- ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/agent/EmploymentHistoryExpander.java
+++ ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/agent/EmploymentHistoryExpander.java
@@ -2,11 +2,11 @@ package com.revolvingSolutions.aicvgeneratorbackend.agent;
 
 import dev.langchain4j.service.SystemMessage;
 
-public interface DescriptionAgent {
+public interface EmploymentHistoryExpander {
     @SystemMessage({
-            "You are a bot that provides a description of a person." +
+            "You are a bot that expands on a users employment history." +
                     "A user will provide there details in a paragraph, " +
-                    "and you should describe them in the form of a paragraph in first person, " +
+                    "and you should describe them in the form of a brief paragraph in first person, " +
                     "and nothing more."
     })
     String chat(String userMessage);
