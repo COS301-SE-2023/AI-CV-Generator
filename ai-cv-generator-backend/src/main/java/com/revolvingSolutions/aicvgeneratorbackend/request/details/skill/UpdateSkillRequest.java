e5c53c730524091c5d8a4d43daf733d1454ae195
nathanMils <106823768+nathanMils@users.noreply.github.com>
2023-08-29 21:00:54 +0200

    🚧 (api) added Reference/Skill requests

---
+++ ai-cv-generator-backend/src/main/java/com/revolvingSolutions/aicvgeneratorbackend/request/details/skill/UpdateSkillRequest.java
@@ -0,0 +1,15 @@
+package com.revolvingSolutions.aicvgeneratorbackend.request.details.skill;
+
+import com.revolvingSolutions.aicvgeneratorbackend.model.user.Skill;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateSkillRequest {
+    private Skill skill;
+}
