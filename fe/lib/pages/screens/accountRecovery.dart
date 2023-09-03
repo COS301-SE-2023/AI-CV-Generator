78f7c54754c45c09ecea7cae4992f7e41f41bcc3
nathanMils <106823768+nathanMils@users.noreply.github.com>
2023-09-03 13:52:01 +0200

    ✨ (ui) added Account recovery Page

---
+++ fe/lib/pages/screens/accountRecovery.dart
@@ -0,0 +1,17 @@
+import 'package:ai_cv_generator/pages/screens/underContruction.dart';
+import 'package:flutter/widgets.dart';
+
+class Recover extends StatefulWidget {
+  const Recover({super.key});
+
+  @override
+  State<StatefulWidget> createState() => RecoverState();
+}
+
+class RecoverState extends State<Recover> {
+  @override
+  Widget build(BuildContext context) {
+    return const UnderContruction();
+  }
+
+}
