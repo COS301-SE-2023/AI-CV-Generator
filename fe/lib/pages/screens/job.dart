3b0acec40ee91e70a3cb669635d0dd527d4de066
u19284072 <u19284072@tuks.co.za>
2023-09-05 16:06:07 +0200

    made fonts bold for title

--- fe/lib/pages/screens/job.dart
+++ fe/lib/pages/screens/job.dart
@@ -133,7 +133,7 @@ class CreateJobCardState extends State<CreateJobCard> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Text(widget.title ?? "N/A", style: TextStyle(fontSize: 16)),
+                  Text(widget.title ?? "N/A", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
                   SizedBox(height: 4),
                   Text(widget.subtitle ?? "N/A", style: TextStyle(fontSize: 14),),
                   SizedBox(height: 8,),
@@ -142,7 +142,7 @@ class CreateJobCardState extends State<CreateJobCard> {
                     color: Colors.grey,
                   ),
                   SizedBox(height: 8,),
-                  Text(widget.location ?? "N/A", style: TextStyle(fontSize: 12)),
+                  Text(widget.location ?? "N/A", style: TextStyle(fontSize: 12,)),
                 ],
               ),
             ),
