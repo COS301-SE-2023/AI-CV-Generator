007f2f074d7b6df58c61884e95d8d42dfdef6672
nathanMils <106823768+nathanMils@users.noreply.github.com>
2023-09-05 14:25:17 +0200

    ✨ (ui) added policy and TandC dialog stubs

---
+++ fe/lib/pages/widgets/policy.dart
@@ -0,0 +1,10 @@
+import 'package:ai_cv_generator/pages/screens/underContruction.dart';
+import 'package:flutter/material.dart';
+
+class Policy extends StatelessWidget {
+  const Policy({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const UnderContruction();
+  }
+}
