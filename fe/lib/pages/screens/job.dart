986db371b85325c6940630167a3427116bf5e7f5
u19284072 <u19284072@tuks.co.za>
2023-09-05 10:51:19 +0200

    added stub for job cards

--- fe/lib/pages/screens/job.dart
+++ fe/lib/pages/screens/job.dart
@@ -22,13 +22,13 @@ class JobsPageState extends State<JobsPage> {
             Navigator.pop(context);
           },
         ),
-      actions: [
-        IconButton(onPressed: () {}, 
-        icon: const Icon(Icons.account_circle, size: 32,)),
-        const SizedBox(width: 16,)
-      ],
+        actions: [
+          IconButton(onPressed: () {}, 
+          icon: const Icon(Icons.account_circle, size: 32,)),
+          const SizedBox(width: 16,)
+        ],
       ),
-      body: const SafeArea(
+      body: SafeArea(
         child: Stack(
           children: [
             Breadcrumb(previousPage: "Home", currentPage: "Jobs",),
@@ -37,7 +37,53 @@ class JobsPageState extends State<JobsPage> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  
+                  Card(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+                    child: Container(
+                      padding: EdgeInsets.all(16),
+                      width: 150,
+                      height: 250,
+                      child: Column(
+                      // mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Expanded(
+                          flex: 2,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text("Title"),
+                              SizedBox(height: 4),
+                              Text("SubTitle"),
+                              SizedBox(height: 8,),
+                              Container(
+                                height: 1,
+                                color: Colors.grey,
+                              ),
+                              SizedBox(height: 8,),
+                              Text("Location"),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          child: SizedBox(
+                            child: Column(
+                              children: [
+                                Text("Salary"),
+                                SizedBox(height: 24,),
+                                ElevatedButton(
+                                  onPressed: () {
+
+                                  }, 
+                                  child: Text("VISIT"),
+                                ),
+                              ],
+                            )
+                        )
+                      ),
+                      ],
+                    ),
+                    )
+                  )
                 ],
               ),
             )
@@ -45,5 +91,19 @@ class JobsPageState extends State<JobsPage> {
         )
       )
     );
+  }
+}
+
+class CreateJobCard extends StatefulWidget {
+  const CreateJobCard({super.key});
+
+  @override
+  CreateJobCardState createState() => CreateJobCardState();
+}
+
+class CreateJobCardState extends State<CreateJobCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Card();
   }
 }
