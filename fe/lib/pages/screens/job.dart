97b959e1c02210e45905d28c8e81d69ad108506b
u19284072 <u19284072@tuks.co.za>
2023-09-05 11:04:57 +0200

    updated job card stub

--- fe/lib/pages/screens/job.dart
+++ fe/lib/pages/screens/job.dart
@@ -37,53 +37,7 @@ class JobsPageState extends State<JobsPage> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Card(
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
-                    child: Container(
-                      padding: EdgeInsets.all(16),
-                      width: 150,
-                      height: 250,
-                      child: Column(
-                      // mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Expanded(
-                          flex: 2,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Text("Title"),
-                              SizedBox(height: 4),
-                              Text("SubTitle"),
-                              SizedBox(height: 8,),
-                              Container(
-                                height: 1,
-                                color: Colors.grey,
-                              ),
-                              SizedBox(height: 8,),
-                              Text("Location"),
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          child: SizedBox(
-                            child: Column(
-                              children: [
-                                Text("Salary"),
-                                SizedBox(height: 24,),
-                                ElevatedButton(
-                                  onPressed: () {
-
-                                  }, 
-                                  child: Text("VISIT"),
-                                ),
-                              ],
-                            )
-                        )
-                      ),
-                      ],
-                    ),
-                    )
-                  )
+                  CreateJobCard()
                 ],
               ),
             )
@@ -95,7 +49,12 @@ class JobsPageState extends State<JobsPage> {
 }
 
 class CreateJobCard extends StatefulWidget {
-  const CreateJobCard({super.key});
+  String? title = "N/A";
+  String? subtitle = "N/A";
+  String? location = "N/A";
+  String? salary = "N/A";
+  String? link = "N/A";
+  CreateJobCard({super.key, this.title, this.subtitle, this.location, this.salary, this.link});
 
   @override
   CreateJobCardState createState() => CreateJobCardState();
@@ -103,7 +62,62 @@ class CreateJobCard extends StatefulWidget {
 
 class CreateJobCardState extends State<CreateJobCard> {
   @override
+  void initState() {
+    widget.title = "N/A";
+    widget.subtitle = "N/A";
+    widget.location = "N/A";
+    widget.salary = "N/A";
+    widget.link = "N/A";
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Card();
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        width: 150,
+        height: 250,
+        child: Column(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(widget.title!),
+                  SizedBox(height: 4),
+                  Text(widget.subtitle!),
+                  SizedBox(height: 8,),
+                  Container(
+                    height: 1,
+                    color: Colors.grey,
+                  ),
+                  SizedBox(height: 8,),
+                  Text(widget.location!),
+                ],
+              ),
+            ),
+            Expanded(
+              child: SizedBox(
+                child: Column(
+                  children: [
+                    Text(widget.salary!),
+                    SizedBox(height: 24,),
+                    ElevatedButton(
+                      onPressed: () {
+
+                      }, 
+                      child: Text("VISIT"),
+                    ),
+                  ],
+                )
+              )
+            ),
+          ],
+        ),
+      )
+    );
   }
 }
