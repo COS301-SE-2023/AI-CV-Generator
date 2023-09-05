6352627185411026fe01469e71dccf5b91207ec7
u19284072 <u19284072@tuks.co.za>
2023-09-05 16:04:35 +0200

    updated card title size

--- fe/lib/pages/screens/job.dart
+++ fe/lib/pages/screens/job.dart
@@ -122,7 +122,7 @@ class CreateJobCardState extends State<CreateJobCard> {
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
       child: Container(
         padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-        width: 200,
+        width: 210,
         height: 300,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
