9eaaaa4ab2b0a1b9a952bb836cd7361ce7468037
nathanMils <106823768+nathanMils@users.noreply.github.com>
2023-09-07 18:57:25 +0200

    ✨ (UI) new snackbar popup placehodler

---
+++ fe/lib/pages/util/errorMessage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ErrorMessage extends StatelessWidget {
+  const ErrorMessage({super.key,required this.message});
+  final String message;
+  @override
+  Widget build(BuildContext context) {
+    return SnackBar(content: Text(message));
+  }
+  
+}
