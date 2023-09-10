bb8b62d4ee42f6647f1fab5a5cff831a3600cb9e
nathanMils <106823768+nathanMils@users.noreply.github.com>
2023-09-10 11:50:54 +0200

    ✨ (ai) chat bot succesfully moved to the backend

--- fe/lib/main.dart
+++ fe/lib/main.dart
@@ -13,10 +13,8 @@ import 'package:ai_cv_generator/pages/screens/profile.dart';
 // external
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
-  await dotenv.load(fileName: ".env");
   Uri myurl = Uri.base;
   if (myurl.path.contains("/share/")) {
     String uuid = myurl.pathSegments.last;
--- fe/lib/pages/util/chatBot.dart
+++ fe/lib/pages/util/chatBot.dart
@@ -1,28 +1,15 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:langchain/langchain.dart';
-import 'package:langchain_openai/langchain_openai.dart';
+import 'package:ai_cv_generator/dio/client/AIApi.dart';
 class Chatbot {
-
-  final bot = ChatOpenAI(
-    apiKey: dotenv.env['OPENAI_KEY'],
-    temperature: double.parse(dotenv.env['OPENAI_TEMP']??"1"),
-    maxTokens: int.parse(dotenv.env['OPENAI_MAX']??"256"),
-    model: dotenv.env['OPENAI_MODEL']??"gpt-3.5-turbo"
-  );
 
   bool ready = false;
 
-  List<ChatMessage> messages = [
-    ChatMessage.system(
-      'You are a helpful chatbot that advises users on creating a cv as well as helping a user navigate our web application. A user will ask a question in the form of a paragraph, and you should answer there question in the form of a paragraph with a word count under 30. The web application starts on the home page where a user can provide their information manually by clicking "Survey" or upload a CV have their information extracted for them by clicking "Upload". The user may also navigate to the profile page by clicking on the button in the top right corner. On the profile page the user can access, store and edit their information.'     ),
-  ];
+  List<String> messages = [];
 
   Future<String> greeting() async {
     try {
-      messages.add(ChatMessage.human("Hello"));
-      var response = await bot.call(messages);
-      messages.add(response);
-      return response.content;
+      var response = await AIApi.chat(messages: messages, userMessage: "Hello");
+      messages = response!;
+      return messages.last;
     } on Exception catch (e) {
       print("Unable to request response from AI chatbot");
       return "Sorry this isnt working";
@@ -33,12 +20,11 @@ class Chatbot {
     required String userMsg
   }) async {
     try {
-      messages.add(ChatMessage.human(userMsg));
-      var response  = await bot.call(messages);
-      messages.add(response);
-      return response.content;
+      var response = await AIApi.chat(messages: messages, userMessage: userMsg);
+      messages = response!;
+      return messages.last;
     } on Exception catch (e) {
-      print("Unable to request response from AI chatbot");
+      print("Unable to request response from AI chatbot");;
       return "Sorry this isnt working";
     }
   }
