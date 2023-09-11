d8b913b25f30186860a7020f0bdc5ff14f0f321a
u19284072 <u19284072@tuks.co.za>
2023-09-11 17:31:13 +0200

    updated jobs ui

--- fe/lib/pages/screens/job.dart
+++ fe/lib/pages/screens/job.dart
@@ -2,9 +2,11 @@ import 'package:ai_cv_generator/dio/client/WebScraperApi.dart';
 import 'package:ai_cv_generator/dio/client/userApi.dart';
 import 'package:ai_cv_generator/models/user/UserModel.dart';
 import 'package:ai_cv_generator/models/webscraper/JobResponseDTO.dart';
+import 'package:ai_cv_generator/pages/elements/elements.dart';
 import 'package:ai_cv_generator/pages/widgets/breadcrumb.dart';
 import 'package:ai_cv_generator/pages/widgets/loadingscreens/loadingScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:universal_html/html.dart';
 
 import 'package:url_launcher/url_launcher.dart';
 
@@ -27,9 +29,18 @@ class JobsPageState extends State<JobsPage> {
   void populate() async {
     UserModel? user = await userApi.getUser();
     if(user != null) {
-        List<JobResponseDTO>? jobs = await getJobs("accounting", user.location ?? "");
-        //List<JobResponseDTO>? jobs = await getRecommended();
+        List<JobResponseDTO>? jobs = await getJobs("accounting", "Pretoria");
+        // List<JobResponseDTO>? jobs = await getRecommended();
         setState(() {
+          // for(int i = 0; i < 10; i++)
+          // {
+          //   jobCards.add(CreateJobCard(
+          //     title: "Cashier",
+          //     subtitle: "Shoprite",
+          //     location: "Pretoria, Gauteng",
+          //     salary: "R10000 - R16000 per month",
+          //   ));
+          // }
           createCards(jobs);
         });
     }
@@ -46,6 +57,7 @@ class JobsPageState extends State<JobsPage> {
   createCards(List<JobResponseDTO>? jobs) {
     if(jobs != null) {
       jobs.forEach((element) {
+        print(element.imgLink);
         jobCards.add(
           CreateJobCard(
             title: element.title,
@@ -53,6 +65,7 @@ class JobsPageState extends State<JobsPage> {
             location: element.location,
             salary: element.salary,
             link: element.link,
+            // imageLink: element.imgLink,
           )
         );
       });
@@ -61,9 +74,9 @@ class JobsPageState extends State<JobsPage> {
 
   @override
   Widget build(BuildContext build) {
-    if(jobCards.isEmpty == true) {
-      return LoadingScreen();
-    }
+    // if(jobCards.isEmpty == true) {
+    //   return LoadingScreen();
+    // }
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
@@ -85,11 +98,15 @@ class JobsPageState extends State<JobsPage> {
         child: Column(
           children: [
             Breadcrumb(previousPage: "Home", currentPage: "Jobs",),
+            Text("RECOMMENDED JOBS FOR YOU", style: TextStyle(fontSize: 24),),
+            SizedBox(height: 24,),
+            // if()
             Expanded(
               child: SingleChildScrollView( 
                 child: Center(
                   child: Container(
-                    width: 1100,
+                    alignment: Alignment.center,
+                    width: 1800,
                     child: Wrap(
                       crossAxisAlignment: WrapCrossAlignment.center,
                       // mainAxisAlignment: MainAxisAlignment.center,
@@ -115,7 +132,8 @@ class CreateJobCard extends StatefulWidget {
   String? location;
   String? salary;
   String? link;
-  CreateJobCard({super.key, this.title, this.subtitle, this.location, this.salary, this.link});
+  String? imageLink;
+  CreateJobCard({super.key, this.title, this.subtitle, this.location, this.salary, this.link, this.imageLink});
 
   @override
   CreateJobCardState createState() => CreateJobCardState();
@@ -128,38 +146,60 @@ class CreateJobCardState extends State<CreateJobCard> {
     return Card(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
       child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-        width: 210,
-        height: 350,
+        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+        width: 400,
+        height: 300,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Expanded(
               flex: 2,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(widget.title ?? "N/A", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-                  SizedBox(height: 4),
-                  Text(widget.subtitle ?? "N/A", style: TextStyle(fontSize: 14),),
-                  SizedBox(height: 8,),
-                  Container(
-                    height: 1,
-                    color: Colors.grey,
-                  ),
-                  SizedBox(height: 8,),
-                  Text(widget.location ?? "N/A", style: TextStyle(fontSize: 12,)),
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Theme.of(context).colorScheme.surface
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(widget.subtitle ?? "N/A", style: TextStyle(fontSize: 14),),
+                          Text(widget.title ?? "N/A", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold,)),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.pink,
+                        backgroundImage: NetworkImage(widget.link!),
+                      )
+                    )
                 ],
+              ),
               ),
             ),
             Expanded(
-              child: SizedBox(
-                child: Column(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    SizedBox(height: 16,),
-                    Text(widget.salary ?? "N/A", style: TextStyle(color: Colors.green),),
-                    SizedBox(height: 24,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(widget.salary ?? "N/A", style: TextStyle(color: Colors.green),),
+                        Text(widget.location ?? "N/A", style: TextStyle(fontSize: 12,)),
+                      ],
+                    ),
                     ElevatedButton(
                       onPressed: () {
                         if(widget.link != null) {
