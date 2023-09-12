9833716fd9b9622296b4938db8118059fc584cad
nathanMils <106823768+nathanMils@users.noreply.github.com>
2023-09-12 13:28:27 +0200

    ✨ (ui) added forgot and change page stubs

---
+++ fe/lib/pages/screens/changePassword.dart
@@ -0,0 +1,16 @@
+import 'package:ai_cv_generator/pages/screens/underContruction.dart';
+import 'package:flutter/cupertino.dart';
+
+class ChangePassword extends StatefulWidget {
+  const ChangePassword({super.key});
+
+  @override
+  State<StatefulWidget> createState() => ChangePasswordState();
+}
+
+class ChangePasswordState extends State<ChangePassword> {
+  @override
+  Widget build(BuildContext context) {
+    return const UnderContruction();
+  }
+}
---
+++ fe/lib/pages/screens/forgotPassword.dart
@@ -0,0 +1,16 @@
+import 'package:ai_cv_generator/pages/screens/underContruction.dart';
+import 'package:flutter/cupertino.dart';
+
+class ForgotPassword extends StatefulWidget {
+  const ForgotPassword({super.key});
+
+  @override
+  State<StatefulWidget> createState() => ForgotPasswordState();
+}
+
+class ForgotPasswordState extends State<ForgotPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return const UnderContruction();
+  }
+}
