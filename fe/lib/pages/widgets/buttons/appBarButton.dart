0e5295f686e7f3e2c0b662c0375f7ac367f20857
nathanMils <106823768+nathanMils@users.noreply.github.com>
2023-09-14 13:24:22 +0200

    ✨ (ui) added app bar theme button

---
+++ fe/lib/pages/widgets/buttons/appBarButton.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class AppBarButtonStyle extends StatelessWidget {
+  const AppBarButtonStyle({super.key, required this.text});
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    double w = screenSize.width/100;
+    double h = screenSize.height/100; 
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Container(
+            height: 4.5*h,
+            width: 80,
+            decoration: BoxDecoration(
+              gradient: const LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Colors.white,
+                    Colors.white
+                  ]),
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  text,
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Theme.of(context).colorScheme.primary
+                    ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
