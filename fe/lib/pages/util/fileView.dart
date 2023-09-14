72801562c7e13fd27482ba020fbbb3b5c1d085e5
nathanMils <106823768+nathanMils@users.noreply.github.com>
2023-09-14 20:41:47 +0200

    🚧 (ui) improved fileView UI

--- fe/lib/pages/util/fileView.dart
+++ fe/lib/pages/util/fileView.dart
@@ -12,57 +12,60 @@ class FileView extends StatelessWidget {
     Size screenSize = MediaQuery.of(context).size;
     double w = screenSize.width/100;
     double h = screenSize.height/100;
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
+    return SizedBox(
+      width: 37*w,
+      child: Scaffold(
         backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          }, 
-          icon: const Icon(Icons.arrow_back,color: Colors.white,)
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            }, 
+            icon: const Icon(Icons.arrow_back,color: Colors.white,)
+          ),
         ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              padding: EdgeInsets.all(w*1),
-              width: 32*w,
-              height: 85*h,
-              child: SfPdfViewer.memory(
-                file!.bytes!,
-                pageSpacing: 8
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.all(w*1),
+                width: 32*w,
+                height: 85*h,
+                child: SfPdfViewer.memory(
+                  file!.bytes!,
+                  pageSpacing: 8
+                ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.all(w*0.1),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: EdgeInsets.only(
-                      right: h*1
+              Container(
+                padding: EdgeInsets.all(w*0.1),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(
+                        right: h*1
+                      ),
+                      child: DownloadButton(file: file,),
                     ),
-                    child: DownloadButton(file: file,),
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(
-                      left: h*1
-                    ),
-                    child: ShareButton(file: file,),
-                  )
-                ],
-              ),
-            )
-          ],
+                    Container(
+                      padding: EdgeInsets.only(
+                        left: h*1
+                      ),
+                      child: ShareButton(file: file,),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-      ),
+      )
     );
   }
 }
