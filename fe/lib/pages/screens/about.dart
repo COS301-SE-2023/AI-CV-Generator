41d748f35be7119396868433dea17e79a81b0977
u19284072 <u19284072@tuks.co.za>
2023-09-15 17:15:58 +0200

    Made about page more responsive

--- fe/lib/pages/screens/about.dart
+++ fe/lib/pages/screens/about.dart
@@ -32,29 +32,34 @@ class AboutPageState extends State<AboutPage> {
       ],
       ),
       body: SafeArea(
-        child: Stack(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
           children: [
             const Breadcrumb(previousPage: "Home", currentPage: "About",),
-            Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
+            Expanded(
+              child: ListView(
                 children: [
-                  const Text("ABOUT US", style: TextStyle(fontSize: 60),),
-                  const SizedBox(height: 24,),
+                  const Text(
+                    "ABOUT US", 
+                    style: TextStyle(fontSize: 60),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
                   Container(
+                    width: 200,
+                    padding: const EdgeInsets.symmetric(horizontal: 90),
                     decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.surface,
+                      // color: Theme.of(context).colorScheme.surface,
                       borderRadius: BorderRadius.circular(20.0),
                     ),
-                    padding: const EdgeInsets.all(24),
-                    width: 600,
                     child: const Text(
                       style: TextStyle(fontSize: 18), 
-                      "A challenge for job seekers in South Africa is the creation of an effective CV, cover letter, or email that can make them stand out to potential employers. Many job seekers lack the necessary knowledge and skills to craft high-quality job application documents that highlight their strengths and experiences. The AI CV Generator aims to aid job seekers in creating appealing job application documents that will increase their chances of acquiring a job."
+                      "A challenge for job seekers in South Africa is the creation of an effective CV, cover letter, or email that can make them stand out to potential employers. Many job seekers lack the necessary knowledge and skills to craft high-quality job application documents that highlight their strengths and experiences. The AI CV Generator aims to aid job seekers in creating appealing job application documents that will increase their chances of acquiring a job.",
+                      textAlign: TextAlign.center,
                     ),
                   ),
-                  const SizedBox(height: 200,),
                 ],
               ),
             )
