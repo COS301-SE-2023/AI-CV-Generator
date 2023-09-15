c02111a0bd98e52e96662f2aca99a8f3610610d3
nathanMils <106823768+nathanMils@users.noreply.github.com>
2023-09-15 09:53:13 +0200

    ✨ (UI) button with onTap parameter for ease of use

---
+++ fe/lib/pages/widgets/buttons/customizableButton.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class CustomizableButton extends StatelessWidget {
+  const CustomizableButton({
+    super.key, 
+    required this.text, 
+    required this.width, 
+    required this.height,
+    required this.onTap
+  });
+  final String text;
+  final double width;
+  final double height;
+  final GestureTapCallback onTap;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: InkWell(
+        onTap: onTap,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Container(
+                height: width,
+                width: height,
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Color(0xFFFDA187),
+                        Color(0xFFEA6D79),
+                      ]),
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      text,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white
+                        ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
