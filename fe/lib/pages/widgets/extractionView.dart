4415f7a6c2dbe45e89759670e2df1a5cd82ec712
u19284072 <u19284072@tuks.co.za>
2023-09-18 09:39:45 +0200

    Extraction view is editable

--- fe/lib/pages/widgets/extractionView.dart
+++ fe/lib/pages/widgets/extractionView.dart
@@ -1,3 +1,9 @@
+import 'package:ai_cv_generator/models/aimodels/AIEmployment.dart';
+import 'package:ai_cv_generator/models/aimodels/AIInput.dart';
+import 'package:ai_cv_generator/models/aimodels/AILink.dart';
+import 'package:ai_cv_generator/models/aimodels/AIQualification.dart';
+import 'package:ai_cv_generator/models/aimodels/AIReference.dart';
+import 'package:ai_cv_generator/models/aimodels/AISkill.dart';
 import 'package:ai_cv_generator/pages/elements/elements.dart';
 import 'package:ai_cv_generator/pages/widgets/buttons/customizableButton.dart';
 import 'package:file_picker/file_picker.dart';
@@ -5,7 +11,78 @@ import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 class ExtractionView {
-  Future<bool> showModal(BuildContext context, PlatformFile file, Map<String,dynamic> data) async {
+  Map controllers = {};
+
+  AIInput update(AIInput aiInput) {
+    aiInput.firstname = controllers["firstname"].text;
+    aiInput.lastname = controllers["lastname"].text;
+    aiInput.email = controllers["email"].text;
+    aiInput.phoneNumber = controllers["phoneNumber"].text;
+    aiInput.location = controllers["location"].text;
+    aiInput.description = controllers["description"].text;
+
+    List<AIEmployment> aiEmploymentList = [];
+    for(int i = 0; i < controllers["experience"].length; i++) {
+      aiEmploymentList.add(
+        AIEmployment(
+          company: controllers["experience"][i]["company"].text,
+          jobTitle: controllers["experience"][i]["jobTitle"].text,
+          startDate: controllers["experience"][i]["startDate"].text,
+          endDate: controllers["experience"][i]["endDate"].text,
+        )
+      );
+    }
+    aiInput.experience = aiEmploymentList;
+
+    List<AIQualification> aiQualificationList = [];
+    for(int i = 0; i < controllers["qualifications"].length; i++) {
+      aiQualificationList.add(
+        AIQualification(
+          qualification: controllers["qualifications"][i]["qualification"].text,
+          institution: controllers["qualifications"][i]["institution"].text,
+          startDate: controllers["qualifications"][i]["startDate"].text,
+          endDate: controllers["qualifications"][i]["endDate"].text,
+        )
+      );
+    }
+    aiInput.qualifications = aiQualificationList;
+
+    List<AILink> ailinksList = [];
+    for(int i = 0; i < controllers["links"].length; i++) {
+      ailinksList.add(
+        AILink(
+          url: controllers["links"][i]["url"].text,
+        )
+      );
+    }
+    aiInput.links = ailinksList;
+
+    List<AIReference> aiReferenceList = [];
+    for(int i = 0; i < controllers["references"].length; i++) {
+      aiReferenceList.add(
+        AIReference(
+          description: controllers["references"][i]["description"].text,
+          contact: controllers["references"][i]["contact"].text,
+        )
+      );
+    }
+    aiInput.references = aiReferenceList;
+
+    List<AISkill> aiskillsList = [];
+    for(int i = 0; i < controllers["skills"].length; i++) {
+      aiskillsList.add(
+        AISkill(
+          skill: controllers["skills"][i]["skill"].text,
+          level: controllers["skills"][i]["level"].text,
+          reason: controllers["skills"][i]["reason"].text,
+        )
+      );
+    }
+    aiInput.skills = aiskillsList;
+    return aiInput;
+  }
+
+  Future<bool> showModal(BuildContext context, PlatformFile file, AIInput aiInput) async {
     bool proceed = false;
     await showDialog(
       context: context,
@@ -47,7 +124,7 @@ class ExtractionView {
                           child: ListView(
                             padding: const EdgeInsets.only(right: 16),
                             children: [
-                              ...extractedData(data)
+                              ...extractedData(aiInput.toJson())
                             ],
                           ),
                         ),
@@ -55,6 +132,12 @@ class ExtractionView {
                         Expanded(
                           child: CustomizableButton(
                             onTap: () {
+                              try {
+                              update(aiInput);
+                                
+                              } catch (e){
+                                print(e);
+                              }
                               proceed = true;
                               Navigator.pop(context);
                             },
@@ -76,15 +159,27 @@ class ExtractionView {
     return proceed;
   }
 
-  List<Widget> extractedData(Map data) {
+  int calculateLines(String text) {
+    int lines = (text.split(" ").length/10).toInt();
+    lines < 1 ? lines = 1 : lines;
+    return lines;
+  }
+
+  List<Widget> extractedData(Map aiInput) {
     List<Widget> widgets = [];
-    data.forEach((key, value) {
+    aiInput.forEach((key, value) {
       widgets.add(SectionHeading(text: key.toString().toUpperCase()));
       if(value is List) {
+        controllers[key] = [];
         for(int i = 0; i < value.length; i++) {
-          value[i].toJson().forEach((key, data) {
+          controllers[key].add({});
+          value[i].toJson().forEach((listKey, data) {
             if(data!= null) {
-              widgets.add(Text(data));
+              TextEditingController controller = TextEditingController();
+              controllers[key][i][listKey] = controller;
+              controller.text = data;
+
+              widgets.add(TextField(controller: controller, maxLines: calculateLines(data),));
             }
           });
           if(i != value.length-1) {
@@ -93,7 +188,10 @@ class ExtractionView {
         }
       }
       else {
-        widgets.add(Text(value.toString()));
+        TextEditingController controller = TextEditingController();
+        controllers[key] = controller;
+        controller.text = value;
+        widgets.add(TextField(controller: controller, maxLines: calculateLines(value),));
       }
       
       widgets.add(const SizedBox(height: 24,));
