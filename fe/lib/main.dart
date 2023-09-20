35f2f7092a8eece7eb590ed56fc915b5d0bcbaa1
nathanMils <106823768+nathanMils@users.noreply.github.com>
2023-09-20 19:13:30 +0200

    🧪 (UI) created fo test

--- fe/lib/main.dart
+++ fe/lib/main.dart
@@ -57,7 +57,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: route,
       routes: {
-        '/':(context) => const Login(),
+        '/':(context) => const TestPage(),
         '/register':(context) => const RegisterPage(),
         '/confirm':(context) => const EmailConfirmation(),
         '/home':(context) => const Home(),
