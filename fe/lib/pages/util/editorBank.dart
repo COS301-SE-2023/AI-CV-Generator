859cfbef411b60d786ed98db30b421dc9b568ed8
nathanMils <106823768+nathanMils@users.noreply.github.com>
2023-09-26 12:38:14 +0200

    🚧 (UI) adding undo function to editor

---
+++ fe/lib/pages/util/editorBank.dart
@@ -0,0 +1,38 @@
+import 'package:ai_cv_generator/models/aimodels/CVData.dart';
+import 'package:ai_cv_generator/pages/template/TemplateChoice.dart';
+
+class EditorModel {
+  final CVData data;
+  final TemplateOption templateOption;
+  final ColorSet colors;
+
+  EditorModel({
+    required this.data,
+    required this.templateOption,
+    required this.colors
+  });
+}
+
+class EditorBank {
+  List<EditorModel> past = [];
+  int currentSize = 0;
+  final int max;
+  
+  add(EditorModel model) {
+    if (currentSize == max) {
+      past.removeAt(0);
+      currentSize--;
+    }
+    past.add(model);
+    currentSize++;
+  }
+
+  EditorModel undo() {
+    return past.removeAt(currentSize--);
+  }
+  
+  EditorBank({
+    required this.past,
+    required this.max
+  });
+}
