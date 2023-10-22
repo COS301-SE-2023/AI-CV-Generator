b4ebc79834d72b554809fd439950dcbd836ca97c
Amanda9805 <akhuzwayo98@gmail.com>
2023-10-22 18:13:58 +0200

    Update view_widgets_test.dart

--- fe/test/view_widgets_test.dart
+++ fe/test/view_widgets_test.dart
@@ -57,6 +57,7 @@ void main(){
 
   group("Test chatbot", () {
     testWidgets('Chatbot window', (WidgetTester tester) async {
+      await tester.runAsync(() async {
         await tester.pumpWidget(
         const MaterialApp(
             home: Scaffold(
@@ -81,6 +82,7 @@ void main(){
 
         expect(find.byIcon(Icons.send_rounded), findsOneWidget);
         expect(find.byIcon(Icons.close), findsOneWidget);
+      });
     });
 
     testWidgets('Message input', (WidgetTester tester) async {
